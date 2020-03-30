98dc670e323f8dfd4ad6c4bdda0764c9ab57c237
patient-computer-3602 <thelionking898@gmail.com>
2020-03-30 15:43:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,8 +14,8 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
-  return array;
+ array.push(element);
+ return array
 }
 
 function accessElementInArray(array, index) {
