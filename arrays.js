9f504c6701a8fe4428bf64733c163f92e8d1610d
patient-computer-3602 <thelionking898@gmail.com>
2020-03-30 15:43:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(element) {
   array.unshift(element);
   return array;
 }
